bdcfc202f50afc98577564fca804371ee4eb09b5
daniel <skyrimdaniel11@gmail.com>
2025-12-20 22:57:23 -0300

    feat: add restriction to password

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
         $validatedFields = $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required'
+            'password' => 'required|min:6'
         ]);
 
         //create new user   
